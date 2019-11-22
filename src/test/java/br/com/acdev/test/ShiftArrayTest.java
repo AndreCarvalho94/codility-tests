37ed1939f88dc2b493f752633c73c57c26d7a99e
André Carvalho <andre.carvalhop94@gmail.com>
2019-11-22 19:05:27 -0300

    odd && shift

--- src/test/java/br/com/acdev/test/ShiftArrayTest.java
+++ src/test/java/br/com/acdev/test/ShiftArrayTest.java
@@ -4,6 +4,7 @@ import br.com.acdev.ShiftArray;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 import static org.junit.Assert.*;
@@ -15,14 +16,8 @@ public class ShiftArrayTest {
         ShiftArray shiftArray = new ShiftArray();
         int[] A = new int[]{3, 8, 9, 7, 6};
         int[] R = new int[]{9, 7, 6, 3, 8};
-        Assert.assertTrue(compare(R, shiftArray.shiftArray(A, 3)));
+        Assert.assertArrayEquals(R, shiftArray.shiftArray(A, 3));
     }
 
-    public boolean compare(int[] A, int[] B){
-        for(int i = 0; i < A.length; i++){
-            if(A[i]!=B[i])
-                return false;
-        }
-        return true;
-    }
+
 }
